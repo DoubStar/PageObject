6e14f26c9e2e9e4d7b583e7d496814f70b2c8236
jiangliancheng <jlc_1981@qq.com>
2017-01-10 13:40:32 +0800

    add properties

---
+++ src/com/jlc/base/ReadProperties.java
@@ -0,0 +1,26 @@
+package com.jlc.base;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadProperties {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Properties prop = new Properties();
+			InputStream in = ReadProperties.class.getClassLoader().getResourceAsStream("user.properties");
+			prop.load(in);
+			System.out.println("直接输出prop对象："+prop);
+			String loginname = prop.getProperty("loginname");
+			String Password = prop.getProperty("Password");
+			System.out.println("loginname:"+loginname+"---"+"Password:"+Password);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+		}
+	}
+
+}
